06b6ee59da76a710aca24afdcf5cbf5d1de23431
axel <axel.mastroianni@incloudteam.com>
2023-01-18 16:36:07 +0100

    Fix Jenkinsfile

--- SsoCommands.groovy
+++ SsoCommands.groovy
@@ -17,28 +17,28 @@ def setEnvironment() {
 def initializeImages() {
     sso_commands_delete_img = sso_commands_delete_registry + ":${env.BUILD_ID}"
     sso_commands_upsert_img = sso_commands_upsert_registry + ":${env.BUILD_ID}"
-    println("${sso_commands_delete_img}")
-    println("${sso_commands_upsert_img}")
+    println("${this.sso_commands_delete_img}")
+    println("${this.sso_commands_upsert_img}")
 }
 
 def buildDockerImages() {
-    ssoCommandsUpsertImage = docker.build("${sso_commands_upsert_img}", "-f ${env.WORKSPACE}/sso_commands/populate_index/Dockerfile .")
-    ssoCommandsDeleteImage = docker.build("${sso_commands_delete_img}", "-f ${env.WORKSPACE}/sso_commands/delete_index/Dockerfile .")
+    ssoCommandsUpsertImage = docker.build("${this.sso_commands_upsert_img}", "-f ${env.WORKSPACE}/sso_commands/populate_index/Dockerfile .")
+    ssoCommandsDeleteImage = docker.build("${this.sso_commands_delete_img}", "-f ${env.WORKSPACE}/sso_commands/delete_index/Dockerfile .")
 }
 
 def runDockerImages() {
-    powershell "docker run --name ${sso_commands_delete} ${sso_commands_delete_img}"
-    powershell "docker run --name ${sso_commands_upsert} ${sso_commands_upsert_img}"
+    powershell "docker run --name ${this.sso_commands_delete} ${this.sso_commands_delete_img}"
+    powershell "docker run --name ${this.sso_commands_upsert} ${sso_commands_upsert_img}"
 }
 
 def stopDockerContainers() {
-    powershell "docker stop ${sso_commands_delete}"
-    powershell "docker stop ${sso_commands_upsert}"
+    powershell "docker stop ${this.sso_commands_delete}"
+    powershell "docker stop ${this.sso_commands_upsert}"
 }
 
 def deleteDockerContainers() {
-    powershell "docker rm ${sso_commands_delete}"
-    powershell "docker rm ${sso_commands_upsert}"
+    powershell "docker rm ${this.sso_commands_delete}"
+    powershell "docker rm ${this.sso_commands_upsert}"
 }
 
 return this
