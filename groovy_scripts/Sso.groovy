89bab0f5641b0b39214f52263be13677ca57db8f
axel <axel.mastroianni@incloudteam.com>
2023-01-19 14:36:58 +0100

    Parametrized Jenkinsfile

--- groovy_scripts/Sso.groovy
+++ groovy_scripts/Sso.groovy
@@ -7,23 +7,15 @@ class Sso {
 
     public void setEnvironment(def script) {
         if (script.params.is_sso_commands) {
-//             ssoCommands = commandsScript.createSsoCommands()
-            ssoCommands = new SsoCommands()
             ssoCommands.setEnvironment()
         }
         if (script.params.is_sso_auth_logs) {
-//             ssoCommands = commandsScript.createSsoCommands()
-            ssoAuthLogs = new SsoAuthLogs()
             ssoAuthLogs.setEnvironment()
         }
         if (script.params.is_sso_user_logs) {
-//             ssoCommands = commandsScript.createSsoCommands()
-            ssoUserLogs = new SsoUserLogs()
             ssoUserLogs.setEnvironment()
         }
         if (script.params.is_sso_users) {
-//             ssoCommands = commandsScript.createSsoCommands()
-            ssoUsers = new SsoUsers()
             ssoUsers.setEnvironment()
         }
     }
