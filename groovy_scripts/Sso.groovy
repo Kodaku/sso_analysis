1526aeceffb5e768e20d5d095d4c0cd6f87d81bc
axel <axel.mastroianni@incloudteam.com>
2023-01-19 14:56:38 +0100

    Parametrized Jenkinsfile

--- groovy_scripts/Sso.groovy
+++ groovy_scripts/Sso.groovy
@@ -5,7 +5,6 @@ class Sso {
     def ssoUsers
 
     public void setup(def script) {
-        GroovyShell shell = new GroovyShell()
         def ssoCommandsScript = script.load "./groovy_scripts/SsoCommands.groovy"
         def ssoAuthLogsScript = script.load "./groovy_scripts/SsoAuthLogs.groovy"
         def ssoUserLogsScript = script.load "./groovy_scripts/SsoUserLogs.groovy"
@@ -14,10 +13,6 @@ class Sso {
         ssoAuthLogs = ssoAuthLogsScript.createSsoAuthLogs()
         ssoUserLogs = ssoUserLogsScript.createSsoUserLogs()
         ssoUsers = ssoUsersScript.createSsoUsers()
-//         ssoCommands = shell.parse(new File('${script.env.WORKSPACE}/groovy_scripts/SsoCommands.groovy')).createSsoCommands()
-//         ssoAuthLogs = shell.parse(new File('${script.env.WORKSPACE}/groovy_scripts/SsoAuthLogs.groovy')).createSsoAuthLogs()
-//         ssoUserLogs = shell.parse(new File('${script.env.WORKSPACE}/groovy_scripts/SsoUserLogs.groovy')).createSsoUserLogs()
-//         ssoUsers = shell.parse(new File('${script.env.WORKSPACE}/groovy_scripts/SsoUsers.groovy')).createSsoUsers()
     }
 
     public void setEnvironment(def script) {
