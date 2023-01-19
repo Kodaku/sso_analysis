e603d5eb354bd6d079625778bb0155b5250f6a8c
axel <axel.mastroianni@incloudteam.com>
2023-01-19 14:35:46 +0100

    Parametrized Jenkinsfile

--- groovy_scripts/Sso.groovy
+++ groovy_scripts/Sso.groovy
@@ -1,13 +1,9 @@
-import SsoCommands
-import SsoAuthLogs
-import SsoUserLogs
-import SsoUsers
-
 class Sso {
-    SsoCommands ssoCommands
-    SsoAuthLogs ssoAuthLogs
-    SsoUserLogs ssoUserLogs
-    SsoUsers ssoUsers
+    GroovyShell shell = new GroovyShell()
+    def ssoCommands = shell.parse(new File('SsoCommands.groovy')).createSsoCommands()
+    def ssoAuthLogs = shell.parse(new File('SsoAuthLogs.groovy')).createSsoAuthLogs()
+    def ssoUserLogs = shell.parse(new File('SsoUserLogs.groovy')).createSsoUserLogs()
+    def ssoUsers = shell.parse(new File('SsoUsers.groovy')).createSsoUsers()
 
     public void setEnvironment(def script) {
         if (script.params.is_sso_commands) {
